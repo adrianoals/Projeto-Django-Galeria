695fc313f6f5dfefc5bbac549ede7a1585cfd573
Adriano Lima <dri.limasantos@gmail.com>
2023-11-06 17:44:19 -0300

    up

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-        return render(request, 'galeria/index.html')
+        dados = {
+                1: {"nome":"Nebulosa de Carina", "legenda":"webbtelescope.org / NASA / James Webb"},
+                2: {"nome":"Galáxia NGC 1079",
+                    "legenda":"nasa.org / NASA / Hubble"}
+                }
+        return render(request, 'galeria/index.html', {"cards": dados} )
+
 
 def imagem(request):
         return render(request, 'galeria/imagem.html')
